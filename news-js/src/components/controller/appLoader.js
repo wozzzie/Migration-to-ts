02da537b11bb02b81493433b6e16f21d9632eaa6
Maria Gornashevich <lightbrown9874@gmail.com>
2022-12-06 17:00:19 +0300

    feat: add api key

--- news-js/src/components/controller/appLoader.js
+++ news-js/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ce76279315204d40a32e3791a75a6767',
         });
     }
 }
